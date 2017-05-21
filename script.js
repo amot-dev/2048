fbdb1930ce1b115cf95d473134c2c97754ff7723
asd1o1 <asd777101@gmail.com>
2017-05-20 20:55:53 -0700

    Add adding functionality

--- script.js
+++ script.js
@@ -4,30 +4,39 @@ $(document).ready(function() {
     }
 
     addUnit();
-    //addUnit();
+    addUnit();
 
     $(document).keyup(function(e) {
         var thisUnit = $(".unit");
         var dir = null;
         var num = null;
+        var num2 = null;
         if (e.keyCode == 38) {  //UP
             for (i = 0; i < 16; i++) {
                 dir = findDirections(i);
                 if (dir.u < 16) {
                     //check that the current unit has content and the one above does not
                     if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.u]).hasClass("empty")) {
-                        console.log("Here: " + dir.h);
-                        console.log("Up: " + dir.u);
-                        console.log("Down: " + dir.d);
-                        console.log("Left: " + dir.l);
-                        console.log("Right: " + dir.r);
-                        console.log(" ");
                         num = $(thisUnit[i]).text();                    //save value of current unit
                         $(thisUnit[dir.u]).toggleClass("empty filled"); //set unit above to filled
                         $(thisUnit[dir.u]).text(num);                   //set value of current to above
 
                         $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
                         $(thisUnit[i]).text(" ");                       //clear value of current
+                        i = 0;                                          //reset loop because action has been made
+                    }
+
+                    //check that the current unit has content and the one above does too
+                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.u]).hasClass("filled")) {
+                        num = $(thisUnit[i]).text();                    //save value of current unit
+                        num2 = $(thisUnit[dir.u]).text();               //save value of unit above
+                        if (num == num2) {
+                            $(thisUnit[dir.u]).text((num * 2));         //set value of current * 2 to above
+
+                            $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
+                            $(thisUnit[i]).text(" ");                   //clear value of current
+                            i = 0;                                      //reset loop because action has been made
+                        }
                     }
                 }
             }
@@ -38,18 +47,26 @@ $(document).ready(function() {
                 if (dir.d < 16) {
                     //check that the current unit has content and the one below does not
                     if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.d]).hasClass("empty")) {
-                        console.log("Here: " + dir.h);
-                        console.log("Up: " + dir.u);
-                        console.log("Down: " + dir.d);
-                        console.log("Left: " + dir.l);
-                        console.log("Right: " + dir.r);
-                        console.log(" ");
                         num = $(thisUnit[i]).text();                    //save value of current unit
                         $(thisUnit[dir.d]).toggleClass("empty filled"); //set unit below to filled
                         $(thisUnit[dir.d]).text(num);                   //set value of current to below
 
                         $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
                         $(thisUnit[i]).text(" ");                       //clear value of current
+                        i = 0;                                          //reset loop because action has been made
+                    }
+
+                    //check that the current unit has content and the one below does too
+                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.d]).hasClass("filled")) {
+                        num = $(thisUnit[i]).text();                    //save value of current unit
+                        num2 = $(thisUnit[dir.d]).text();               //save value of unit below
+                        if (num == num2) {
+                            $(thisUnit[dir.d]).text((num * 2));         //set value of current * 2 to below
+
+                            $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
+                            $(thisUnit[i]).text(" ");                   //clear value of current
+                            i = 0;                                      //reset loop because action has been made
+                        }
                     }
                 }
             }
@@ -60,18 +77,26 @@ $(document).ready(function() {
                 if (dir.l < 16) {
                     //check that the current unit has content and the one to the left does not
                     if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.l]).hasClass("empty")) {
-                        console.log("Here: " + dir.h);
-                        console.log("Up: " + dir.u);
-                        console.log("Down: " + dir.d);
-                        console.log("Left: " + dir.l);
-                        console.log("Right: " + dir.r);
-                        console.log(" ");
                         num = $(thisUnit[i]).text();                    //save value of current unit
                         $(thisUnit[dir.l]).toggleClass("empty filled"); //set unit to the left to filled
                         $(thisUnit[dir.l]).text(num);                   //set value of current to left
 
                         $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
                         $(thisUnit[i]).text(" ");                       //clear value of current
+                        i = 0;                                          //reset loop because action has been made
+                    }
+
+                    //check that the current unit has content and the one to the left does too
+                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.l]).hasClass("filled")) {
+                        num = $(thisUnit[i]).text();                    //save value of current unit
+                        num2 = $(thisUnit[dir.l]).text();               //save value of unit to the left
+                        if (num == num2) {
+                            $(thisUnit[dir.l]).text((num * 2));         //set value of current * 2 to left
+
+                            $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
+                            $(thisUnit[i]).text(" ");                   //clear value of current
+                            i = 0;                                      //reset loop because action has been made
+                        }
                     }
                 }
             }
@@ -82,22 +107,31 @@ $(document).ready(function() {
                 if (dir.r < 16) {
                     //check that the current unit has content and the one to the right does not
                     if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.r]).hasClass("empty")) {
-                        console.log("Here: " + dir.h);
-                        console.log("Up: " + dir.u);
-                        console.log("Down: " + dir.d);
-                        console.log("Left: " + dir.l);
-                        console.log("Right: " + dir.r);
-                        console.log(" ");
                         num = $(thisUnit[i]).text();                    //save value of current unit
                         $(thisUnit[dir.r]).toggleClass("empty filled"); //set unit to the right to filled
                         $(thisUnit[dir.r]).text(num);                   //set value of current to right
 
                         $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
                         $(thisUnit[i]).text(" ");                       //clear value of current
+                        i = 0;                                          //reset loop because action has been made
+                    }
+
+                    //check that the current unit has content and the one to the right does too
+                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.r]).hasClass("filled")) {
+                        num = $(thisUnit[i]).text();                    //save value of current unit
+                        num2 = $(thisUnit[dir.r]).text();               //save value of unit to the right
+                        if (num == num2) {
+                            $(thisUnit[dir.r]).text((num * 2));         //set value of current * 2 to right
+
+                            $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
+                            $(thisUnit[i]).text(" ");                   //clear value of current
+                            i = 0;                                      //reset loop because action has been made
+                        }
                     }
                 }
             }
         }
+        addUnit();                                                      //add another unit
     })
 
     function findDirections(gridNum) {

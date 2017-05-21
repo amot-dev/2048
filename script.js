1c14319f8a858d218dab7dce0161334e8b6a91d6
asd1o1 <asd777101@gmail.com>
2017-05-20 21:08:49 -0700

    Fix #1 Realised that moving the value of a square forward would get it to be updated again in the loop and made the loop restart every change in the last commit. That didn't work so now it restarts after a full loop, but only if there were changes made.

--- script.js
+++ script.js
@@ -11,125 +11,146 @@ $(document).ready(function() {
         var dir = null;
         var num = null;
         var num2 = null;
+        var changesMade = false;
         if (e.keyCode == 38) {  //UP
-            for (i = 0; i < 16; i++) {
-                dir = findDirections(i);
-                if (dir.u < 16) {
-                    //check that the current unit has content and the one above does not
-                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.u]).hasClass("empty")) {
-                        num = $(thisUnit[i]).text();                    //save value of current unit
-                        $(thisUnit[dir.u]).toggleClass("empty filled"); //set unit above to filled
-                        $(thisUnit[dir.u]).text(num);                   //set value of current to above
+            do {
+                changesMade = false;                                        //reset changes made value
+                for (i = 0; i < 16; i++) {
+                    dir = findDirections(i);
+                    if (dir.u < 16) {
+                        //check that the current unit has content and the one above does not
+                        if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.u]).hasClass("empty")) {
+                            num = $(thisUnit[i]).text();                    //save value of current unit
+                            $(thisUnit[dir.u]).toggleClass("empty filled"); //set unit above to filled
+                            $(thisUnit[dir.u]).text(num);                   //set value of current to above
 
-                        $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
-                        $(thisUnit[i]).text(" ");                       //clear value of current
-                        i = 0;                                          //reset loop because action has been made
-                    }
+                            $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
+                            $(thisUnit[i]).text(" ");                       //clear value of current
 
-                    //check that the current unit has content and the one above does too
-                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.u]).hasClass("filled")) {
-                        num = $(thisUnit[i]).text();                    //save value of current unit
-                        num2 = $(thisUnit[dir.u]).text();               //save value of unit above
-                        if (num == num2) {
-                            $(thisUnit[dir.u]).text((num * 2));         //set value of current * 2 to above
+                            changesMade = true;                             //will redo the loop once it's done
+                        }
 
-                            $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
-                            $(thisUnit[i]).text(" ");                   //clear value of current
-                            i = 0;                                      //reset loop because action has been made
+                        //check that the current unit has content and the one above does too
+                        if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.u]).hasClass("filled")) {
+                            num = $(thisUnit[i]).text();                    //save value of current unit
+                            num2 = $(thisUnit[dir.u]).text();               //save value of unit above
+                            if (num == num2) {
+                                $(thisUnit[dir.u]).text((num * 2));         //set value of current * 2 to above
+
+                                $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
+                                $(thisUnit[i]).text(" ");                   //clear value of current
+
+                                changesMade = true;                             //will redo the loop once it's done
+                            }
                         }
                     }
                 }
-            }
+            } while (changesMade)
         }
         if (e.keyCode == 40) {  //DOWN
-            for (i = 0; i < 16; i++) {
-                dir = findDirections(i);
-                if (dir.d < 16) {
-                    //check that the current unit has content and the one below does not
-                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.d]).hasClass("empty")) {
-                        num = $(thisUnit[i]).text();                    //save value of current unit
-                        $(thisUnit[dir.d]).toggleClass("empty filled"); //set unit below to filled
-                        $(thisUnit[dir.d]).text(num);                   //set value of current to below
+            do {
+                changesMade = false;                                        //reset changes made value
+                for (i = 0; i < 16; i++) {
+                    dir = findDirections(i);
+                    if (dir.d < 16) {
+                        //check that the current unit has content and the one below does not
+                        if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.d]).hasClass("empty")) {
+                            num = $(thisUnit[i]).text();                    //save value of current unit
+                            $(thisUnit[dir.d]).toggleClass("empty filled"); //set unit below to filled
+                            $(thisUnit[dir.d]).text(num);                   //set value of current to below
 
-                        $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
-                        $(thisUnit[i]).text(" ");                       //clear value of current
-                        i = 0;                                          //reset loop because action has been made
-                    }
+                            $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
+                            $(thisUnit[i]).text(" ");                       //clear value of current
 
-                    //check that the current unit has content and the one below does too
-                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.d]).hasClass("filled")) {
-                        num = $(thisUnit[i]).text();                    //save value of current unit
-                        num2 = $(thisUnit[dir.d]).text();               //save value of unit below
-                        if (num == num2) {
-                            $(thisUnit[dir.d]).text((num * 2));         //set value of current * 2 to below
+                            changesMade = true;                             //will redo the loop once it's done
+                        }
 
-                            $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
-                            $(thisUnit[i]).text(" ");                   //clear value of current
-                            i = 0;                                      //reset loop because action has been made
+                        //check that the current unit has content and the one below does too
+                        if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.d]).hasClass("filled")) {
+                            num = $(thisUnit[i]).text();                    //save value of current unit
+                            num2 = $(thisUnit[dir.d]).text();               //save value of unit below
+                            if (num == num2) {
+                                $(thisUnit[dir.d]).text((num * 2));         //set value of current * 2 to below
+
+                                $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
+                                $(thisUnit[i]).text(" ");                   //clear value of current
+
+                                changesMade = true;                             //will redo the loop once it's done
+                            }
                         }
                     }
                 }
-            }
+            } while (changesMade)
         }
         if (e.keyCode == 37) {  //LEFT
-            for (i = 0; i < 16; i++) {
-                dir = findDirections(i);
-                if (dir.l < 16) {
-                    //check that the current unit has content and the one to the left does not
-                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.l]).hasClass("empty")) {
-                        num = $(thisUnit[i]).text();                    //save value of current unit
-                        $(thisUnit[dir.l]).toggleClass("empty filled"); //set unit to the left to filled
-                        $(thisUnit[dir.l]).text(num);                   //set value of current to left
+            do {
+                changesMade = false;                                        //reset changes made value
+                for (i = 0; i < 16; i++) {
+                    dir = findDirections(i);
+                    if (dir.l < 16) {
+                        //check that the current unit has content and the one to the left does not
+                        if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.l]).hasClass("empty")) {
+                            num = $(thisUnit[i]).text();                    //save value of current unit
+                            $(thisUnit[dir.l]).toggleClass("empty filled"); //set unit to the left to filled
+                            $(thisUnit[dir.l]).text(num);                   //set value of current to left
 
-                        $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
-                        $(thisUnit[i]).text(" ");                       //clear value of current
-                        i = 0;                                          //reset loop because action has been made
-                    }
+                            $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
+                            $(thisUnit[i]).text(" ");                       //clear value of current
 
-                    //check that the current unit has content and the one to the left does too
-                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.l]).hasClass("filled")) {
-                        num = $(thisUnit[i]).text();                    //save value of current unit
-                        num2 = $(thisUnit[dir.l]).text();               //save value of unit to the left
-                        if (num == num2) {
-                            $(thisUnit[dir.l]).text((num * 2));         //set value of current * 2 to left
+                            changesMade = true;                             //will redo the loop once it's done
+                        }
 
-                            $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
-                            $(thisUnit[i]).text(" ");                   //clear value of current
-                            i = 0;                                      //reset loop because action has been made
+                        //check that the current unit has content and the one to the left does too
+                        if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.l]).hasClass("filled")) {
+                            num = $(thisUnit[i]).text();                    //save value of current unit
+                            num2 = $(thisUnit[dir.l]).text();               //save value of unit to the left
+                            if (num == num2) {
+                                $(thisUnit[dir.l]).text((num * 2));         //set value of current * 2 to left
+
+                                $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
+                                $(thisUnit[i]).text(" ");                   //clear value of current
+
+                                changesMade = true;                             //will redo the loop once it's done
+                            }
                         }
                     }
                 }
-            }
+            } while (changesMade);
         }
         if (e.keyCode == 39) {  //RIGHT
-            for (i = 0; i < 16; i++) {
-                dir = findDirections(i);
-                if (dir.r < 16) {
-                    //check that the current unit has content and the one to the right does not
-                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.r]).hasClass("empty")) {
-                        num = $(thisUnit[i]).text();                    //save value of current unit
-                        $(thisUnit[dir.r]).toggleClass("empty filled"); //set unit to the right to filled
-                        $(thisUnit[dir.r]).text(num);                   //set value of current to right
+            do {
+                changesMade = false;                                        //reset changes made value
+                for (i = 0; i < 16; i++) {
+                    dir = findDirections(i);
+                    if (dir.r < 16) {
+                        //check that the current unit has content and the one to the right does not
+                        if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.r]).hasClass("empty")) {
+                            num = $(thisUnit[i]).text();                    //save value of current unit
+                            $(thisUnit[dir.r]).toggleClass("empty filled"); //set unit to the right to filled
+                            $(thisUnit[dir.r]).text(num);                   //set value of current to right
 
-                        $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
-                        $(thisUnit[i]).text(" ");                       //clear value of current
-                        i = 0;                                          //reset loop because action has been made
-                    }
+                            $(thisUnit[i]).toggleClass("filled empty");     //set current to empty
+                            $(thisUnit[i]).text(" ");                       //clear value of current
 
-                    //check that the current unit has content and the one to the right does too
-                    if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.r]).hasClass("filled")) {
-                        num = $(thisUnit[i]).text();                    //save value of current unit
-                        num2 = $(thisUnit[dir.r]).text();               //save value of unit to the right
-                        if (num == num2) {
-                            $(thisUnit[dir.r]).text((num * 2));         //set value of current * 2 to right
+                            changesMade = true;                             //will redo the loop once it's done
+                        }
 
-                            $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
-                            $(thisUnit[i]).text(" ");                   //clear value of current
-                            i = 0;                                      //reset loop because action has been made
+                        //check that the current unit has content and the one to the right does too
+                        if ($(thisUnit[i]).hasClass("filled") && $(thisUnit[dir.r]).hasClass("filled")) {
+                            num = $(thisUnit[i]).text();                    //save value of current unit
+                            num2 = $(thisUnit[dir.r]).text();               //save value of unit to the right
+                            if (num == num2) {
+                                $(thisUnit[dir.r]).text((num * 2));         //set value of current * 2 to right
+
+                                $(thisUnit[i]).toggleClass("filled empty"); //set current to empty
+                                $(thisUnit[i]).text(" ");                   //clear value of current
+
+                                changesMade = true;                             //will redo the loop once it's done
+                            }
                         }
                     }
                 }
-            }
+            } while (changesMade);
         }
         addUnit();                                                      //add another unit
     })
